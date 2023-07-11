e2d0c1efa85e403f20c6cbd2d4a802610ef41de9
Thanteeeeee <139093769+Thanteeeeee@users.noreply.github.com>
2023-07-11 09:07:13 +0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 { 
 
-  cout<<"   #####    "<<endl;
+  cout<<"   ####    "<<endl;
   cout<<"  #     #   "<<endl;
   cout<<"  #         "<<endl;
   cout<<"  #         "<<endl;
   cout<<"  #         "<<endl;
   cout<<"  #     #   "<<endl;
-  cout<<"   #####    "<<endl;
+  cout<<"   ####    "<<endl;
   
 
     system("pause");
